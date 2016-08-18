eeb5680de091938554f6953b1fd8fc67826910b0
Ignas2526 <ignas2526@gmail.com>
2016-08-18 16:12:15 +0700

    Update UoP Enhancer to 0.1.0

--- UoP-ehancer.meta.js
+++ UoP-ehancer.meta.js
@@ -3,15 +3,18 @@
 // @author      Ignas Poklad (Ignas2526)
 // @namespace   ignas2526_uopeople_moodle_enhancer
 // @description Enhances UoPeople Moodle
+// @version     0.1.0
+// @downloadURL https://raw.githubusercontent.com/Ignas2526/UoP-Enahncer/master/UoP-ehancer.user.js
+// @updateURL https://raw.githubusercontent.com/Ignas2526/UoP-Enahncer/master/UoP-ehancer.meta.js
+// @run-at document-start
 // @include     http://my.uopeople.edu/*
 // @include     https://my.uopeople.edu/*
-// @version     0.0.2
+// @connect     my.uopeople.edu
+// @connect     capi.grammarly.com
+// @grant       unsafeWindow
 // @grant       GM_xmlhttpRequest
 // @grant       GM_log
 // @grant       GM_addStyle
 // @grant       GM_setValue
 // @grant       GM_getValue
-// @run-at      document-start
-// @downloadURL https://raw.githubusercontent.com/Ignas2526/UoP-Enahncer/master/UoP-ehancer.user.js
-// @updateURL https://raw.githubusercontent.com/Ignas2526/UoP-Enahncer/master/UoP-ehancer.meta.js
 // ==/UserScript==
